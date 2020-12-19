0edbad5cdbc51cda42df20e0b5c58a72bf54f94d
nightrooster52 <jordanjalles@gmail.com>
2020-12-18 19:12:44 -0800

    Add files via upload

---
+++ Scripts/QuadrupedAgent.cs
@@ -0,0 +1,337 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+
+public class QuadrupedAgent : Agent
+{
+
+    public Transform target;
+    public float positionSpringPower = 100;
+    public float maxSpringForce = 200;
+    private float initialMaxSpringForce;
+    public Transform[] limbs;
+    public float minUpVectorDot;
+    public Transform chest;
+    Rigidbody rBody;
+    public float distanceToTouchTarget = 1f;
+    public float targetDistanceRange = 18f;
+    public bool immortal = false;
+    public bool randomizeMaxSpringForce = false;
+    public bool randomizeLegScales = false;
+    public float frontScale = 0.75f;
+    public float backScale = 0.75f;
+    private float efficiencyRollingAverage = 0;
+    private float efficiencyRollingAverageDepth = 10; 
+    private Vector3 velocityRollingAverage = Vector3.zero;
+    private float velocityRollingAverageDepth = 20; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        this.initialMaxSpringForce = maxSpringForce;
+
+        for (int i = 0; i < limbs.Length; i++)
+        {
+            ConfigurableJoint cJoint = limbs[i].GetComponent<ConfigurableJoint>();
+            SetJointDriveMaximumForce(cJoint, maxSpringForce);
+        }
+
+        float previousDistanceToTarget = Vector3.Distance(this.transform.localPosition, target.localPosition);
+
+    }
+
+
+
+    //Used to reset a training scenario
+    //randomize goal location
+    //reset position and orientation of agent
+    //etc...
+    public override void OnEpisodeBegin()
+    {
+        RandomizeTargetLocation();
+        while (DistanceToTarget() < distanceToTouchTarget * 3)
+        {
+            RandomizeTargetLocation();
+        }
+
+        if (Vector3.Dot(this.transform.up, Vector3.up) < minUpVectorDot || chest.GetComponent<TouchingGround>().touching || GetComplacency() > 0.5f)
+        {
+            this.rBody.transform.rotation = Quaternion.identity;
+            this.rBody.velocity = Vector3.zero;
+            this.rBody.angularVelocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0.07f, 0);
+            this.rBody.transform.Rotate(new Vector3(0, Random.value * 360, 0));
+            for (int i = 0; i < limbs.Length; i++)
+            {
+                //Debug.Log(limbs[i].name);
+                limbs[i].rotation = Quaternion.identity;
+                
+                float xRot = 0;
+
+                if (limbs[i].name == "upper")
+                {
+                    xRot = -45;
+                }
+                if (limbs[i].name == "lower")
+                {
+                    xRot = 45;
+                }
+                limbs[i].Rotate(new Vector3(xRot, 0, 0));
+            }
+        }
+
+        if (randomizeMaxSpringForce)
+        {
+            this.maxSpringForce = this.initialMaxSpringForce * RandomGaussian();
+            Debug.Log(this.maxSpringForce / this.initialMaxSpringForce);
+        }
+
+        if (randomizeLegScales)
+        {
+            Transform backRight = limbs[0].transform.parent;
+            Transform backLeft = limbs[2].transform.parent;
+            Transform frontRight = limbs[4].transform.parent;
+            Transform frontLeft = limbs[6].transform.parent;
+
+            //float frontScale = RandomGaussian()*2f + 0.5f;
+            //float backScale = RandomGaussian()*2f + 0.5f;
+            frontScale = 1.2f;
+            backScale = 1.2f;
+
+            ResizeLimbParent(backRight, backScale);
+            ResizeLimbParent(backLeft, backScale);
+            ResizeLimbParent(frontRight, frontScale);
+            ResizeLimbParent(frontLeft, frontScale);
+
+        }
+    }
+
+    private void ResizeLimbParent(Transform limbParent, float newScale)
+    {
+        //assuming uniform scale in all dimensions for now
+        
+        float oldScale = limbParent.localScale.x;
+        /*
+        limbParent.localScale = (Vector3.one * newScale);
+
+        ConfigurableJoint[] joints = limbParent.GetComponentsInChildren<ConfigurableJoint>();
+        */
+        foreach (Transform limb in limbParent.GetComponentsInChildren<Transform>())
+        {
+            limb.localScale *= newScale / oldScale;
+            limb.GetComponentInChildren<ConfigurableJoint>().autoConfigureConnectedAnchor = false;
+        }
+    }
+
+    private void RandomizeTargetLocation()
+    {
+        target.localPosition = new Vector3(Random.value * targetDistanceRange - targetDistanceRange/2, 0, Random.value * targetDistanceRange - targetDistanceRange/2);
+    }
+
+    //total observation size 32
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+        
+        //target position vec3
+        sensor.AddObservation(this.transform.InverseTransformPoint(target.position).normalized);
+        
+        //body momentum vec3
+        sensor.AddObservation(rBody.velocity);
+
+        //body angular velocity vec3
+        sensor.AddObservation(rBody.angularVelocity);
+
+        //body rotation vec3
+        sensor.AddObservation(rBody.transform.rotation);
+
+        //facing target
+        sensor.AddObservation(GetFacingTarget());
+
+        //limb positions vec3 * 8
+        //limb rotation vec3 * 8
+        //limb touchign ground bool * 8
+        foreach (Transform limb in limbs)
+        {
+            //limb attributes relative to body
+            sensor.AddObservation(this.transform.InverseTransformPoint(limb.position));
+            sensor.AddObservation(limb.GetComponent<ConfigurableJoint>().slerpDrive.maximumForce / this.maxSpringForce);
+            sensor.AddObservation(limb.transform.localRotation);
+            sensor.AddObservation(this.transform.InverseTransformDirection(limb.GetComponent<Rigidbody>().velocity));
+            sensor.AddObservation(this.transform.InverseTransformDirection(limb.GetComponent<Rigidbody>().angularVelocity));
+            sensor.AddObservation(limb.GetComponent<TouchingGround>().touching);
+        }
+        
+        sensor.AddObservation(this.maxSpringForce);
+        sensor.AddObservation(frontScale);
+        sensor.AddObservation(backScale);
+        
+    }
+
+    //execute the actions given from the brain
+    //update rewards
+    public override void OnActionReceived(float[] vectorAction)
+    {
+
+
+        
+        Vector3 controlSignal = Vector3.zero;
+        float minRotation;
+        float maxRotation;
+        float forceUsedPercent = 0f;
+
+        for (int i = 0; i < limbs.Length; i++) {
+            ConfigurableJoint cJoint = limbs[i].GetComponent<ConfigurableJoint>();
+            minRotation = cJoint.lowAngularXLimit.limit;
+            maxRotation = cJoint.highAngularXLimit.limit;
+            controlSignal.x = Mathf.Lerp(minRotation, maxRotation, Mathf.InverseLerp(-1, 1, vectorAction[(i * 4)]));
+
+            minRotation = -cJoint.angularYLimit.limit;
+            maxRotation = cJoint.angularYLimit.limit;
+            controlSignal.y = Mathf.Lerp(minRotation, maxRotation, Mathf.InverseLerp(-1, 1, vectorAction[(i * 4) + 1]));
+
+            minRotation = -cJoint.angularZLimit.limit;
+            maxRotation = cJoint.angularZLimit.limit;
+            controlSignal.z = Mathf.Lerp(minRotation, maxRotation, Mathf.InverseLerp(-1, 1, vectorAction[(i * 4) + 2]));
+
+            float forceSignal = Mathf.InverseLerp(-1, 1, vectorAction[i * 4 + 3]);
+            forceUsedPercent += forceSignal / limbs.Length;
+
+            float maximumForce = maxSpringForce * forceSignal;
+            SetJointDriveMaximumForce(cJoint, maximumForce);
+
+            cJoint.targetRotation = Quaternion.Euler(controlSignal);
+        }
+
+        
+        
+
+
+        if (chest.GetComponent<TouchingGround>().touching || Vector3.Dot(this.transform.up, Vector3.up) < minUpVectorDot)
+        {
+            //If fallen down or fell over
+            
+            SetReward(-1.0f);
+            if (!immortal)
+            {
+                EndEpisode();
+            }
+        }
+
+        float efficiency = (1 - forceUsedPercent);
+        efficiencyRollingAverage = (efficiencyRollingAverage + (efficiency / efficiencyRollingAverageDepth)) / (1 + 1f / efficiencyRollingAverageDepth);
+        //Debug.Log("e:" + efficiency + " era" + efficiencyRollingAverage.ToString());
+
+        velocityRollingAverage = (velocityRollingAverage + (rBody.velocity / velocityRollingAverageDepth)) / (1 + 1f / velocityRollingAverageDepth);
+        //Debug.Log("v:" + rBody.velocity.ToString() + "vra" + velocityRollingAverage.ToString());
+        float velocityDelta = (velocityRollingAverage - rBody.velocity).magnitude;
+        float movementSmoothness = 1 - (float)System.Math.Tanh(velocityDelta/1.5f);
+        //DrawRedGreen(movementSmoothness*2-1);
+        //Debug.Log("ms: " + movementSmoothness.ToString());
+
+
+        //0 is away from target, 1 is towards target
+        float facingTarget = Mathf.InverseLerp(-1, 1, GetFacingTarget());
+        //float facingTargetPow2 = Mathf.Pow(facingTarget, 2);
+        
+        float velocityTowardsTarget = rBody.velocity.magnitude*Vector3.Dot(rBody.velocity.normalized, (target.position - this.transform.position).normalized);
+        //velocity towards target limit 1
+        float vttl1 = Mathf.Max((float)System.Math.Tanh(velocityTowardsTarget / 6), 0);
+        //Debug.Log("vttl1: " + vttl1.ToString());
+        //DrawRedGreen(vttl1);
+
+        float levelHorizon =  Mathf.InverseLerp(minUpVectorDot, 1f, Vector3.Dot(this.transform.up, Vector3.up));
+        
+        //DrawRedGreen(levelHorizon*2-1);
+        //Debug.Log(levelHorizon);
+
+
+
+        float reward = 0;
+        
+        reward = facingTarget * vttl1 * efficiencyRollingAverage * movementSmoothness * levelHorizon;
+        
+        AddReward(reward); //reward for moving towards goal
+        //Debug.Log("reward:" + reward);
+        //DrawRedGreen(reward);
+
+
+
+
+        //Debug.Log(distanceToTarget);
+        if (DistanceToTarget() < distanceToTouchTarget)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+
+    }
+
+    private float DistanceToTarget()
+    {
+        return Vector3.Distance(this.transform.localPosition, target.localPosition);
+    }
+    private void DrawRedGreen(float reward)
+    {
+
+        //Debug.Log("draw reward: "+ transform.GetComponentsInChildren<Renderer>().ToString());
+        foreach (Renderer rend in transform.GetComponentsInChildren<Renderer>())
+        {
+            Color c = Color.Lerp(Color.red, Color.green, Mathf.InverseLerp(-1, 1, reward));
+            rend.sharedMaterial.SetColor("_EmissionColor", c);
+            rend.sharedMaterial.SetColor("_BaseColor", c);
+        }
+
+
+    }
+
+    private void SetJointDriveMaximumForce(ConfigurableJoint cJoint, float newMaximumForce)
+    {
+        JointDrive jDrive = new JointDrive();
+        jDrive.maximumForce = newMaximumForce;
+        jDrive.positionSpring = this.positionSpringPower;
+        cJoint.slerpDrive = jDrive;
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        for (int i = 0; i < actionsOut.Length; i++)
+        {
+            float v = Mathf.InverseLerp(-1, 1, Input.GetAxis("Horizontal"));
+            float h = Mathf.InverseLerp(-1, 1, Input.GetAxis("Vertical"));
+            //actionsOut[i] = (i % 2 == 0) ? v : h;
+        }
+    }
+
+    private float limit1(float number, float halfWayMark)
+    {
+        return 1 - (1 / (number / halfWayMark + 1));
+    }
+
+    private float limit1(float number)
+    {
+        return limit1(number, 0.5f);
+    }
+
+    public float GetComplacency()
+    {
+        return  Mathf.Pow(1 - (limit1(rBody.velocity.magnitude, 0.25f)), 2);
+    }
+
+    public float GetFacingTarget()
+    {
+        return Vector3.Dot(this.transform.forward, (this.transform.position - target.position).normalized);
+    }
+
+    public float RandomGaussian()
+    {
+        //average of three random values approximates gaussian sample
+        return (Random.value + Random.value + Random.value) / 3f;
+    }
+}
---
+++ Scripts/TouchingGround.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchingGround : MonoBehaviour
+{
+    public bool touching = false;
+    const string k_Target = "ground";
+    // Start is called before the first frame update
+    /// <summary>
+    /// Check for collision with a target.
+    /// </summary>
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.CompareTag(k_Target))
+        {
+            touching = true;
+        }
+    }
+
+    /// <summary>
+    /// Check for end of ground collision and reset flag appropriately.
+    /// </summary>
+    void OnCollisionExit(Collision other)
+    {
+        if (other.transform.CompareTag(k_Target))
+        {
+            touching = false;
+        }
+    }
+
+}
